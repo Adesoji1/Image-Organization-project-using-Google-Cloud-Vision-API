7c920d55dd03f187d8e8ccf3b158e709be2af9fb
Adesoji Alu <adesoji.alu@gmail.com>
2024-06-06 09:55:59 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+KEY_PATH = 'path/to/your/service-account-file.json'
+SOURCE_FOLDER_ID = 'your_source_folder_id'
+CATEGORIES_TO_FOLDERS = {
+    'Cat': 'folder_id_for_cats',
+    'Dog': 'folder_id_for_dogs',
+    # Add more categories and corresponding folder IDs
+}
